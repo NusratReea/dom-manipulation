068662f100fd26fe8e041a7d83e9f00c2046b956
Shahareea Nusrat <shahareeanusrat@gmail.com>
2018-10-24 06:14:39 +0000

    classwork-5

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 /* Task 1 */
-
-var heading=document.getElementById('heading');
+var heading = document.getElementById('heading');
 console.log(heading.innerText);
-/*heading.innerText="Changed it";*/
-/*heading.innerHTML=<h3>"Changed it"</h3>;*/
+heading.innerText="change it";
 /* Task 2 using plain JavaScript */
-var textBtn=document.getElementById('addText2');
+
+var textBtn = document.getElementById('addText2a');
 textBtn.addEventListener('click',function(event){
-   var task2=document.getElementById('task2a');
-   task2.innerText="Hello World";
-   var paragraph = document.createElement('p');
-   paragraph.innerText="Hello World";
-   task2.appendChild('paragraph');
+    var task2=document.getElementById('task2a');
+    var paragraph= document.createElement('p');
+    task2.innerText="hello world";
+    task2.appendChild(paragraph);
+    
 });
 
 function changeBGcolor(event){
-    var body=document.getElementsByTagName('body')[0];
-    if(event.target.innerText=="Red"){
+    var body= document.getElementsByTagName('body')[0];
+    if (event.target.innerText=="Red") {
         body.style.backgroundColor='red';
+        
     }
-    if(event.target.innerText=="Green"){
+     if (event.target.innerText=="Green") {
         body.style.backgroundColor='green';
+        
     }
+    
 }
 
+//task2c
+
+function redirectToGoogle(event){
+    
+    window.location.assign("https://www.google.com/")
+}
 
 /* Task 4 using jQuery */
+//task4a
+
+$('#addText4').click(function(event){
+    
+    var paragraph = $('<p>').text('hello world');
+    $('#task4a').append(paragraph);
+});
+
+//task4b
+
+function changeBG(event){
+    var color = event.target.innerText.toLowerCase ()
+    $('body').css('background-color', color);
+    
+}
+//task4c
+$("#sum").click(function(event){
+var sum = Number($("#num1").val()) +Number ($("#num2").val());
+$('#result').text(sum);
+})
